2261ffb7ff8f1d53cc0512834d481d583acb4425
RedRex100 <129302477+RedRex100@users.noreply.github.com>
2024-12-15 15:31:48 -0300

    Delete Jogo da Velha-Fernando directory

--- Jogo da Velha-Fernando/models/usuario.py
+++
@@ -1,6 +0,0 @@
-from database import database
-
-class Usuario(database.Model):
-    id = database.Column(database.Integer, primary_key = True)
-    name = database.Column(database.String(100), nullable = False)
-    senha = database.Column(database.String(100), nullable = False)
